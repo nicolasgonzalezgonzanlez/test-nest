5c319e3520f2a1f54b21804a15d32451347e5d67
Nicolas Samuel Gonzalez (u632827) <nsgonzalez@teco.com.ar>
2022-10-13 14:10:56 -0300

    FEATURE - BASE

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Example')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('Example')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
